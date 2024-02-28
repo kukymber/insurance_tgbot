ecd8c979e5096b5f3f640775bdc9b6cbd40b65cc
kykumber <ostap_to@mail.ru>
2024-02-28 21:10:51 +0800

    add new file with general_button

---
+++ src/core/general_button.py
@@ -0,0 +1,14 @@
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+
+
+
+def get_step_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard.add("Назад")
+    return keyboard
+
+async def process_back(message: types.Message, state: FSMContext, class_name):
+    await class_name.go_back(state)
+    await message.answer("Вы вернулись на предыдущий шаг.")
