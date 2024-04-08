f7b1bdfd7136c46448b4594d0f2c3439d9e9a564
kykumber <ostap_to@mail.ru>
2024-04-08 21:37:26 +0800

    do report func

--- src/telegram/reports/report.py
+++ src/telegram/reports/report.py
@@ -1,37 +1,33 @@
+import httpx
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from src.core.engine import dp, API_URL
-import httpx
 
-class ReportForm(StatesGroup):
-    choosing_action = State()
-    input_period = State()
-    input_ids_for_extension = State()
+from src.core.engine import API_URL
+from src.telegram.states.state import ReportData
 
 API_URL = "http://127.0.0.1:8000/users/get_all"
 
-@dp.message_handler(commands=['report'], state='*')
+
 async def start_report(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add("Выбрать период", "Отметить продленные")
     await message.answer("Выберите действие:", reply_markup=markup)
-    await ReportForm.choosing_action.set()
 
-@dp.message_handler(lambda message: message.text not in ["Выбрать период", "Отметить продленные"], state=ReportForm.choosing_action)
-async def process_invalid_action(message: types.Message):
-    return await message.reply("Пожалуйста, выберите действие из предложенных кнопок.")
 
-@dp.message_handler(state=ReportForm.choosing_action)
+# @dp.message_handler(lambda message: message.text not in ["Выбрать период", "Отметить продленные"],
+#                     state=ReportData.report_period)
+# async def process_invalid_action(message: types.Message):
+#     return await message.reply("Пожалуйста, выберите действие из предложенных кнопок.")
+
 async def process_action(message: types.Message, state: FSMContext):
     if message.text == "Выбрать период":
-        await ReportForm.input_period.set()
+        await ReportData.input_period.set()
         await message.answer("Введите период в формате дд.мм.гггг")
     elif message.text == "Отметить продленные":
-        await ReportForm.input_ids_for_extension.set()
+        await ReportData.input_ids_for_extension.set()
         await message.answer("Укажите ID полисов через запятую, которые были продлены.")
 
-@dp.message_handler(state=ReportForm.input_period)
+
 async def process_period(message: types.Message, state: FSMContext):
     period = message.text
     async with httpx.AsyncClient() as client:
@@ -40,9 +36,34 @@ async def process_period(message: types.Message, state: FSMContext):
             await message.answer(f"Отчет за период {period}: {response.json()}")
         else:
             await message.answer("Произошла ошибка при получении отчета.")
+            await ReportData.report_action.set()
     await state.finish()
 
-@dp.message_handler(state=ReportForm.input_ids_for_extension)
+
+async def process_client_action(message: types.Message, state: FSMContext):
+    user_id = message.text
+    data = await state.get_data()
+    json_data = {
+                "description": "string",
+                "polis_type": "string",
+                "polis_extended": False,
+                "time_insure_end": "2024-04-08",
+                "time_create": "2024-04-08T13:11:20.023Z",
+                "first_name": "string",
+                "middle_name": "string",
+                "last_name": "string",
+                "phone": "string",
+                "email": "user@example.com"
+                }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{API_URL}users/update/{user_id}/", json=json_data)
+        if response.status_code == 201:
+            await message.answer(f"200")
+        else:
+            await message.answer("Произошла ошибка при получении отчета.")
+            await ReportData.report_action.set()
+    await state.finish()
+
 async def process_ids_for_extension(message: types.Message, state: FSMContext):
     ids = message.text
     await state.finish()
@@ -50,9 +71,8 @@ async def process_ids_for_extension(message: types.Message, state: FSMContext):
 
 def register_report_handlers(dp: Dispatcher):
     from src.telegram.head import Form
-    dp.register_message_handler(start_report, state=Form.report_period)
-    dp.register_message_handler(process_action, state=Form.action)
-    # dp.register_message_handler(process_client_action, state=Form.user_action)
-    # dp.register_message_handler(process_action, state=Form.action)
-    # dp.register_message_handler(process_client_action, state=Form.user_action)
-
+    dp.register_message_handler(start_report, state=ReportData.report_action)
+    dp.register_message_handler(process_action, lambda message: message.text.lower() in
+                                                                ["выбрать период", "отметить продленные"], state="*")
+    dp.register_message_handler(process_period, state=ReportData.input_period)
+    dp.register_message_handler(process_client_action, state=ReportData.input_ids_for_extension)
