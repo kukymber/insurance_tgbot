ba2a5984e126b7ebccec819c97f572ac2034c6b5
kykumber <ostap_to@mail.ru>
2024-05-24 14:02:24 +0800

    added type annotation

--- src/telegram/start.py
+++ src/telegram/start.py
@@ -1,10 +1,12 @@
+from typing import Union
+
 from aiogram import types
 
 from src.telegram.buttons.button import get_main_menu_keyboard
 from src.telegram.states.title import Title
 
 
-async def cmd_start(event):
+async def cmd_start(event: Union[types.Message, types.CallbackQuery]) -> None:
     if isinstance(event, types.Message):
         message = event
     elif isinstance(event, types.CallbackQuery):
