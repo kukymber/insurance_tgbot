4aa8b429dad0143438ed21382d92d50da2cb4f70
kykumber <ostap_to@mail.ru>
2024-05-24 13:53:30 +0800

    added type annotation

--- src/core/general_button.py
+++ src/core/general_button.py
@@ -1,14 +1,15 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup
 
 
-def get_step_keyboard():
+def get_step_keyboard() -> ReplyKeyboardMarkup:
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    keyboard.add("Пердыдуший шаг")
+    keyboard.add("Предыдущий шаг")
     return keyboard
 
 
-async def process_back(message: types.Message, state: FSMContext, class_name):
+async def process_back(message: types.Message, state: FSMContext, class_name: type) -> None:
     await class_name.go_back(state)
     await message.answer("Вы вернулись на предыдущий шаг.")
 
@@ -16,7 +17,9 @@ async def process_back(message: types.Message, state: FSMContext, class_name):
 async def go_back_state(message: types.Message, state: FSMContext) -> None:
     data = await state.get_data()
 
-    if data:
+    if 'previous_state' in data:
         previous_state = data['previous_state']
         await previous_state.set()
         await message.answer("Вы вернулись на предыдущий шаг.")
+    else:
+        await message.answer("Предыдущее состояние не найдено.")
