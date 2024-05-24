6c909e663babfd87817a5e05c81932f72c8b146d
kykumber <ostap_to@mail.ru>
2024-05-24 13:59:19 +0800

    added type annotation

--- src/telegram/buttons/button.py
+++ src/telegram/buttons/button.py
@@ -1,21 +1,21 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-def get_main_menu_keyboard():
+def get_main_menu_keyboard() -> ReplyKeyboardMarkup:
     """Клавиатура главного меню."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(KeyboardButton("Клиент"), KeyboardButton("Отчет"))
     return markup
 
 
-def get_client_action_keyboard():
+def get_client_action_keyboard() -> ReplyKeyboardMarkup:
     """Клавиатура для выбора действий с клиентом."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add("Создать", "Изменить", "Найти", "Назад")
     return markup
 
 
-def get_report_action_keyboard():
+def get_report_action_keyboard() -> ReplyKeyboardMarkup:
     """Клавиатура для выбора действий с отчетами."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add("Выбрать период", "Отметить продленные", "Назад")
