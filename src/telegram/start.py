eed00e525f2b7dced001e048d421d63071832bf6
kykumber <ostap_to@mail.ru>
2024-06-05 23:31:09 +0800

    Change handlers logic(callbackquery)

--- src/telegram/start.py
+++ src/telegram/start.py
@@ -1,18 +1,29 @@
 from typing import Union
 
 from aiogram import Dispatcher, types
-from aiogram.dispatcher import FSMContext
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from src.core.state_manger import go_back
 from src.core.engine import bot, TELEGRAM_CHAT_ID
-from src.telegram.keyboards.keyboards import create_client_menu, create_report_menu, create_main_menu
-from src.telegram.states.client.client_state import UserDataState
+from src.telegram.keyboards.keyboards import create_client_menu, create_main_menu
 from src.telegram.states.report.report_state import ReportData
 from src.telegram.states.title import Title
-from src.telegram.users.client_action import (
-    start_user_data_collection
-)
+from aiogram.dispatcher import FSMContext
+
+
+async def main_menu(callback_query: types.CallbackQuery) -> None:
+    await callback_query.message.edit_text("Главное меню", reply_markup=create_main_menu())
+    await Title.start_action.set()
+
+
+async def client_menu(callback_query: types.CallbackQuery) -> None:
+    await callback_query.message.edit_text("Меню клиента", reply_markup=create_client_menu())
+    await Title.user_action.set()
+
+
+async def report_menu(message: types.Message) -> None:
+    markup = create_client_menu()
+    await message.answer("Меню Клиента", reply_markup=markup)
+    await ReportData.report_action.set()
 
 
 async def send_to_admin(dp: Dispatcher) -> None:
@@ -22,35 +33,7 @@ async def send_to_admin(dp: Dispatcher) -> None:
     await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Бот запущен. Нажмите на кнопку ниже, чтобы начать работу.",
                            reply_markup=start_button)
 
-async def start_bot(callback_query: types.CallbackQuery) -> None:
-    await callback_query.message.answer("Бот начал работу! Добро пожаловать!")
-    await callback_query.answer()
 
+async def cmd_start(callback_query: types.CallbackQuery) -> None:
+    await main_menu(callback_query)
 
-async def process_action(message: types.Message, state: FSMContext) -> None:
-    async with state.proxy() as data:
-        data['action'] = message.text
-        mes = data['action']
-    if mes == "Клиент":
-        await state.update_data(previous_state=Title.start_action)
-        markup = create_client_menu()
-        await Title.user_action.set()
-        await message.answer("Выберите действие с клиентом:", reply_markup=markup)
-    elif mes == "Отчет":
-        data['previous_state'] = message.text
-        markup = create_report_menu()
-        await ReportData.report_action.set()
-        await message.answer("Выберите действие с отчетом:", reply_markup=markup)
-
-
-async def process_client_action(message: types.Message, state: FSMContext, callback_query: CallbackQuery) -> None:
-    if message.text == "Создать":
-        await start_user_data_collection(message, state)
-    elif message.text == "Изменить":
-        await UserDataState.user_id.set()
-        await message.answer("Введите ID клиента, которого нужно изменить.")
-    elif message.text == "Найти":
-        await Title.find_user.set()
-        await message.answer("Введите данные для поиска клиента.")
-    elif message.text == "Назад":
-        await go_back(callback_query)
