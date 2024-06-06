53728061947f6dceccddfd512e7193b4ad040899
kykumber <ostap_to@mail.ru>
2024-06-06 13:17:06 +0800

    Added func check server health

--- src/core/engine.py
+++ src/core/engine.py
@@ -2,6 +2,7 @@
 import logging
 import os
 
+import httpx
 from aiogram import Bot, Dispatcher
 from aiogram import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -31,3 +32,19 @@ def start_bot() -> None:
     from src.telegram.start import send_to_admin
     register_handlers(dp)
     executor.start_polling(dp, on_startup=send_to_admin, skip_updates=True)
+
+
+async def check_server_status():
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{API_URL}/health")
+            if response.status_code == 200 and response.json().get("status") == "ok":
+                return True
+            else:
+                return False
+        except httpx.HTTPStatusError as exc:
+            print(f"HTTP error occurred: {exc}")
+            return False
+        except httpx.RequestError as exc:
+            print(f"An error occurred while requesting {exc.request.url!r}.")
+            return False
