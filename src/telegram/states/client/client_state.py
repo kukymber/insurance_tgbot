079f641eee7a04bda3447c2649104db2810c2995
kykumber <ostap_to@mail.ru>
2024-06-08 17:35:10 +0800

    redo saving previous_client_states and go_back func

--- src/telegram/states/client/client_state.py
+++ src/telegram/states/client/client_state.py
@@ -1,7 +1,10 @@
+from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 from src.core.engine import TELEGRAM_CHAT_ID, bot
+from src.telegram.keyboards.keyboards import create_main_menu
+from src.telegram.states.title import Title
 
 
 class UserDataState(StatesGroup):
@@ -22,23 +25,30 @@ class UserDataState(StatesGroup):
         Сохраняем предыдущее состояние в контексте пользователя.
         """
         async with state.proxy() as data:
-            data['previous_client_state'] = previous_client_state
+            if 'previous_client_states' not in data:
+                data['previous_client_states'] = []
+            data['previous_client_states'].append(previous_client_state)
 
     @classmethod
-    async def go_back(cls, state: FSMContext):
-        data = await state.get_data()
-        if data:
-            key_to_remove = data['previous_client_state'].split(':')[1]
+    async def go_back(cls, callback_query: types.CallbackQuery, state: FSMContext):
+        async with state.proxy() as data:
+            if 'previous_client_states' in data and data['previous_client_states']:
+                previous_client_state = data['previous_client_states'].pop()
+                key_to_remove = previous_client_state.split(':')[1]
 
-            input_text = data.pop(key_to_remove, None)
+                input_text = data.pop(key_to_remove, None)
 
-            if input_text:
-
-                await state.set_data(data)
-                state_to_set = getattr(UserDataState, key_to_remove)
-                await state_to_set.set()
-                await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=f"Вы вводили - '{input_text}'")
+                if input_text:
+                    await state.set_data(data)
+                    state_to_set = getattr(UserDataState, key_to_remove)
+                    await state_to_set.set()
+                    await bot.send_message(chat_id=callback_query.from_user.id, text=f"Вы вводили - '{input_text}'")
+                else:
+                    await state.finish()
             else:
                 await state.finish()
-        else:
-            await state.finish()
+                await bot.send_message(chat_id=callback_query.from_user.id, text="Вы вернулись в главное меню.")
+                await Title.start_action.set()
+                markup = create_main_menu()
+                await callback_query.message.edit_text("Выберите действие:", reply_markup=markup)
+        await bot.answer_callback_query(callback_query.id)
