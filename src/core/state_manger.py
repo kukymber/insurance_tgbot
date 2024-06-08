441d6988e3e2c7ff7354557655305d49bec12542
kykumber <ostap_to@mail.ru>
2024-06-08 18:44:24 +0800

    added logic for working with a specific user states

--- src/core/state_manger.py
+++ src/core/state_manger.py
@@ -1,6 +1,8 @@
 from aiogram import types
+
 from src.core.engine import user_state
 from src.telegram.keyboards.keyboards import create_main_menu, create_client_menu, create_report_menu
+from src.telegram.states.title import Title
 
 MAX_STATE_STACK_SIZE = 10
 
@@ -17,7 +19,7 @@ async def process_callback(callback_query: types.CallbackQuery):
 
     if data in state_function_map:
         await switch_state(user_id, data, callback_query.message)
-    elif data == 'Назад':
+    elif data == 'back':
         await go_back(callback_query)
     else:
         await callback_query.answer(f"Вы выбрали: {data}")
@@ -29,7 +31,10 @@ async def switch_state(user_id: int, new_state: str, message: types.Message):
 
     user_state[user_id].append(new_state)
     await check_and_trim_state_stack(user_id)
-    await state_function_map[new_state](message)
+
+    markup = state_function_map[new_state]()
+    await message.bot.send_message(chat_id=message.chat.id, text="Выберите действие:", reply_markup=markup)
+    await Title.start_action.set()
 
 
 async def go_back(callback_query: types.CallbackQuery):
@@ -38,11 +43,20 @@ async def go_back(callback_query: types.CallbackQuery):
         user_state[user_id].pop()
         if user_state[user_id]:
             previous_state = user_state[user_id][-1]
-            await state_function_map[previous_state](callback_query.message)
+            markup = state_function_map[previous_state]()
+            await callback_query.message.bot.send_message(chat_id=callback_query.message.chat.id,
+                                                          text="Выберите действие:", reply_markup=markup)
         else:
-            await state_function_map['main_menu'](callback_query.message)
+            markup = state_function_map['main_menu']()
+            await callback_query.message.bot.send_message(chat_id=callback_query.message.chat.id,
+                                                          text="Вы вернулись в главное меню.", reply_markup=markup)
+            await Title.start_action.set()
     else:
-        await callback_query.answer("Нет предыдущего состояния!")
+        markup = state_function_map['main_menu']()
+        await callback_query.message.bot.send_message(chat_id=callback_query.message.chat.id,
+                                                      text="Вы вернулись в главное меню.", reply_markup=markup)
+        await Title.start_action.set()
+    await callback_query.answer()
 
 
 async def check_and_trim_state_stack(user_id: int):
