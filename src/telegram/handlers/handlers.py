694fd6525e10e09aaf5c37f14c472eb805184edf
kykumber <ostap_to@mail.ru>
2024-06-08 18:41:29 +0800

    changed the order of handlers

--- src/telegram/handlers/handlers.py
+++ src/telegram/handlers/handlers.py
@@ -1,11 +1,10 @@
 from aiogram import Dispatcher
-
 from src.core.state_manger import process_callback
 from src.telegram.start import cmd_start, main_menu, client_menu, report_menu
 from src.telegram.states.client.client_state import UserDataState
 from src.telegram.states.title import Title
 from src.telegram.users.client_action import (
-    process_back_wrapper, start_user_data_collection,
+    start_user_data_collection,
     process_first_name, process_middle_name, process_last_name, process_phone,
     process_email, process_time_insure_end, process_polis_type, process_description,
     process_user_id
@@ -13,16 +12,14 @@ from src.telegram.users.client_action import (
 
 
 def register_handlers(dp: Dispatcher) -> None:
-    dp.register_callback_query_handler(cmd_start, lambda c: c.data == 'start')
+    dp.register_callback_query_handler(cmd_start, lambda c: c.data == 'start', state='*')
+    dp.register_callback_query_handler(main_menu, lambda c: c.data == 'main_menu', state='*')
     dp.register_callback_query_handler(client_menu, lambda c: c.data == 'client_menu', state=Title.start_action)
     dp.register_callback_query_handler(report_menu, lambda c: c.data == 'report_menu', state=Title.start_action)
+    dp.register_callback_query_handler(start_user_data_collection, lambda c: c.data == 'create',
+                                       state=Title.user_action)
     dp.register_callback_query_handler(UserDataState.go_back, lambda c: c.data == "предыдущий шаг", state=UserDataState)
 
-    dp.register_callback_query_handler(start_user_data_collection,
-                                       lambda c: c.data in "create", state=Title.user_action)
-
-    dp.register_callback_query_handler(main_menu, lambda c: c.data == 'main_menu', state='*')
-    dp.register_callback_query_handler(process_callback, lambda c: c.data == 'back', state='*')
     dp.register_message_handler(process_user_id, lambda c: c.data == 'edit', state=Title.user_action)
     dp.register_message_handler(process_first_name, state=UserDataState.first_name)
     dp.register_message_handler(process_middle_name, state=UserDataState.middle_name)
@@ -32,3 +29,4 @@ def register_handlers(dp: Dispatcher) -> None:
     dp.register_message_handler(process_time_insure_end, state=UserDataState.time_insure_end)
     dp.register_callback_query_handler(process_polis_type, state=UserDataState.polis_type)
     dp.register_message_handler(process_description, state=UserDataState.process_description)
+    dp.register_callback_query_handler(process_callback, state='*', )
